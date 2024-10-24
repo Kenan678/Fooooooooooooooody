beec1e3a904cca084a191ac45f1d0976e90e85ba
Kanan Mammadov <kanan.mammadov@demirorenteknoloji.onmicrosoft.com>
2024-10-24 11:01:15 +0400

    commit6

---
+++ Foody.PresentationLayer/ViewComponents/AdminLayoutViewComponents/_CustomTemplateLayoutComponentPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.ViewComponents.AdminLayoutViewComponents
+{
+	public class _CustomTemplateLayoutComponentPartial:ViewComponent
+	{
+		public IViewComponentResult Invoke()
+		{
+			return View();	
+		}
+
+	}
+}
