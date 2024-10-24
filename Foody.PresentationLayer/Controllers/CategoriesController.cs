237d36708de58ec242b4d317d7c1ad469758d190
Kanan Mammadov <kanan.mammadov@demirorenteknoloji.onmicrosoft.com>
2024-10-24 16:27:02 +0400

    c7

---
+++ Foody.PresentationLayer/Controllers/CategoriesController.cs
@@ -0,0 +1,15 @@
+﻿using Foody.BusinessLayer.Abstract;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Foody.PresentationLayer.Controllers
+{
+	public class CategoriesController : Controller
+	{
+		private	readonly ICategoryService _categoryService;	
+		public IActionResult Index()
+		{
+			return View();
+
+		}
+	}
+}
